8d87656529f7499c98ddd7f56c335e52343a8484
Sudhir Mitharwal <luckysud4@gmail.com>
2023-05-15 11:39:18 +0530

    feat(share-link): add link share

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
-const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.njk", "./src/**/*.md"],
+  content: ["./src/**/*.html", "./src/**/*.njk", "./src/**/*.md"],
   darkMode: "media",
   theme: {
-    colors: {
-      transparent: "transparent",
-      currentColor: "currentColor",
-      whatsapp: {
-        "light-green": "#25D366",
-        "chat-bg": "#e5ddd5",
-        "chat-bg-dark": "#0d1519",
-        "input-bg-dark": "#2c393f",
-        "outgoing-chat-bg": "#DCF8C6",
-        "outgoing-chat-bg-dark": "#056162",
+    extend: {
+      colors: {
+        transparent: "transparent",
+        currentColor: "currentColor",
+        whatsapp: {
+          "light-green": "#25D366",
+          "chat-bg": "#e5ddd5",
+          "chat-bg-dark": "#0d1519",
+          "input-bg-dark": "#2c393f",
+          "outgoing-chat-bg": "#DCF8C6",
+          "outgoing-chat-bg-dark": "#056162",
+        },
       },
-      ...colors,
     },
   },
   variants: {
